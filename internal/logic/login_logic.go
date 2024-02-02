d584888f3b5e6fec4614ea61469598d16d46c9bf
jayyi <jayyi@tencent.com>
2024-02-02 14:34:41 +0800

    Update login_logic.go

--- internal/logic/login_logic.go
+++ internal/logic/login_logic.go
@@ -2,42 +2,72 @@ package logic
 
 import (
 	"context"
+    "encoding/json"
+    "github.com/zeromicro/go-zero/core/logc"
+    "google.golang.org/grpc/status"
 
-	"mooon-login-example/internal/svc"
+    "mooon-login-example/internal/svc"
 	"mooon-login-example/pb/mooon_login"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
+const (
+    EmptyRequest   = 2024020201 // 空的请求
+    InvalidRequest = 2024020202 // 无效请求
+)
+
 type LoginLogic struct {
-	ctx    context.Context
-	svcCtx *svc.ServiceContext
-	logx.Logger
+    ctx    context.Context
+    svcCtx *svc.ServiceContext
+    logx.Logger
+}
+
+// LoginReq 登录请求
+type LoginReq struct {
+    Username string `json:"username"` // 用户名
+    Password string `json:"password"` // 密码
 }
 
 func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic {
-	return &LoginLogic{
-		ctx:    ctx,
-		svcCtx: svcCtx,
-		Logger: logx.WithContext(ctx),
-	}
+    return &LoginLogic{
+        ctx:    ctx,
+        svcCtx: svcCtx,
+        Logger: logx.WithContext(ctx),
+    }
 }
 
 func (l *LoginLogic) Login(in *mooon_login.LoginReq) (*mooon_login.LoginResp, error) {
-	// todo: add your logic here and delete this line
-	logx.Infof("Body: %s\n", in.Body)
-	var loginResp mooon_login.LoginResp
+    // todo: add your logic here and delete this line
+    var loginReq LoginReq
+    var out mooon_login.LoginResp
 
-	_ = in
-	httpCookie := mooon_login.Cookie{
-		Name:  "sid",
-		Value: "example",
-	}
-	loginResp.HttpCookies = append(loginResp.HttpCookies, &httpCookie)
+    // 判断请求是否为空
+    if len(in.Body) == 0 {
+        logc.Error(l.ctx, "empty request")
+        return nil, status.Error(EmptyRequest,"empty request")
+    }
 
-    loginResp.HttpHeaders = make(map[string]string)
-	loginResp.HttpHeaders["Mooon-Header"] = "example"
-	loginResp.Body = []byte("{\"mooon\":\"example\"}")
+    // 解密请求
+    err := json.Unmarshal(in.Body, &loginReq)
+    if err != nil {
+        logc.Errorf(l.ctx, "invalid request")
+        return nil, status.Error(InvalidRequest,"empty request")
+    }
 
-	return &loginResp, nil
+    // 写 cookies
+    httpCookie := mooon_login.Cookie{
+        Name:  "sid",
+        Value: "example",
+    }
+    out.HttpCookies = append(out.HttpCookies, &httpCookie)
+
+    // 写 http 头
+    out.HttpHeaders = make(map[string]string)
+    out.HttpHeaders["Mooon-Header"] = "example"
+
+    // 写响应体
+    out.Body = []byte("{\"mooon\":\"example\"}")
+
+    return &out, nil
 }
